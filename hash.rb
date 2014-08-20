ca470f1d864f5767646af025996f498a1929a5c5
lili545442483 <lili545442483@163.com>
2014-08-20 08:22:09 +0800

    refresh

---
+++ hash.rb
@@ -0,0 +1,10 @@
+pets = Hash.new
+pets["Stevie"] = "cat"
+
+pets["Bowser"] = "hamster"
+
+pets["Kevin Sorbo"] = "fish"
+
+puts pets["Stevie"]
+puts pets["Bowser"]
+puts pets["Kevin Sorbo"]
--- method_include?.rb
+++
@@ -1,9 +0,0 @@
-print "input something here"
-
-user_input = gets.chomp
-
-user_input.downcase!
-
-if user_input.include? "s"
-    print "s included"
-end
