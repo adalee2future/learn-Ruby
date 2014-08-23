8191156928328b85e10315f4281f4cec859e46cc
adalee2future <Ada@AdaLeedeMacBook-Pro.local>
2014-08-23 13:51:52 +0800

    add some code

---
+++ class_examples.rb
@@ -0,0 +1,17 @@
+class Message
+    @@messages_sent = 0
+    def initialize(from, to)
+        @from = from
+        @to = to
+        @@messages_sent += 1
+    end
+    
+end
+
+class Email < Message
+    def initialize(from, to)
+        super(from, to)
+    end
+end
+
+my_message = Message.new("China", "America")
